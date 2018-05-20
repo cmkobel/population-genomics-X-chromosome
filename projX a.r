b9aec93f19d293ed2feffa9dbe0caef0f72ae820
Carl M. Kobel <kobel@pm.me>
2018-05-20 20:12:04 +0200

    plan b works

--- projX a.r
+++ projX a.r
@@ -88,23 +88,9 @@ ggplot(fst_af_we, aes(x=pos)) +
 # --------
 # Identify the 10 strongest Fst outlier regions in each case. (This will be done from the 100 snp windows)
 
-#testet:
-window_size = 100
-adjusted_index_offset =  window_size-(window_size/2+1)
-sorted = sort(fst_af_we_100$sliding_window, index.return = T, decreasing = T)
-sortedn = tibble(x = sorted$x, ix = sorted$ix + adjusted_index_offset, pos = fst_af_we_100$pos[sorted$ix + adjusted_index_offset]) # adjust for window size
-# nu kan det her indsættet i overlap med et range fra 1:n som giver et ønsket antal gener.
-# Det kan godt være at koden kan skrives mere effektivt med argumentet partial (?sort), jeg gider bare ikke teste det.
-
-af_we_all_peak_candidates = tibble(start = sortedn$pos,
-                                   end = sortedn$pos,
-                                   fst_rolwin_hecto = sortedn$x)
 
 
-
-
-
-
+# PLAN A
 # Denne funktion bruges ikke hvis sorteringsmeoden er frugtbar
 cut_fst = function(df_fst, n, return = FALSE) {
     sorted = sort(df_fst$sliding_window, index.return = T, decreasing = T)
@@ -115,14 +101,12 @@ cut_fst = function(df_fst, n, return = FALSE) {
 }
 
 
-    
-
 
 # AF WE
 #cut_fst(fst_af_we, 5000)
 # Comment on window size: Because we want to finde parts of genes having high Fst, we need to look in windows 
 af_we_thresh = 310; cut_fst(fst_af_we_100, af_we_thresh)
-write.table(cut_fst(fst_af_we_100, af_we_thresh, T), file = "10_peaks_fst_af_we.tsv")
+#write.table(cut_fst(fst_af_we_100, af_we_thresh, T), file = "10_peaks_fst_af_we.tsv")
 af_we_peaks = tibble(start = c(19182660, 37878268, 46095748, 55959471, 66270950, 92414472, 104551933, 140734729, 141641747, 145276227),
                      end = start,
                      fst = c(0.2502342793, 0.2194004233, 0.2158843816, 0.2101256287, 0.2144259546, 0.2463066009, 0.2330120671, 0.2172732175, 0.2584422211, 0.214525942))
@@ -131,7 +115,7 @@ plot(af_we_peaks)
 
 # WE EA <
 we_ea_thresh = 700; cut_fst(fst_we_ea_100, we_ea_thresh) # plot
-write.table(cut_fst(fst_we_ea_100, we_ea_thresh, T), file = "10_peaks_fst_we_ea.tsv")
+#write.table(cut_fst(fst_we_ea_100, we_ea_thresh, T), file = "10_peaks_fst_we_ea.tsv")
 we_ea_peaks = tibble(start = c(25671846, 42579633, 71373407, 73295813, 74119733, 87742329, 108285879, 109355662, 116617318, 128684757),
                      end = start,
                       fst = c(0.2626619986, 0.2428685249, 0.2935684424, 0.3036629517, 0.2230540469, 0.2677614183, 0.2119459508, 0.2461968528, 0.2129359005, 0.2161413781))
@@ -140,7 +124,7 @@ plot(we_ea_peaks)
 
 # EA AF
 ea_af_thresh = 870; cut_fst(fst_ea_af_100, ea_af_thresh)
-write.table(cut_fst(fst_ea_af_100, ea_af_thresh, T), file = "10_peaks_fst_ea_af.tsv")
+#write.table(cut_fst(fst_ea_af_100, ea_af_thresh, T), file = "10_peaks_fst_ea_af.tsv")
 ea_af_peaks = tibble(start = c(19183926, 36636363, 55960598, 63969563, 66270950, 90576998, 112894056, 121069027, 124687575, 126784188),
                      end = start,
                      fst = c(0.2697129718, 0.3670665736, 0.2939780025, 0.2757454847, 0.390637325, 0.3747966217, 0.2606229121, 0.2809754149, 0.2643502852, 0.3067912248))
@@ -148,7 +132,41 @@ plot(ea_af_peaks[2:3])
 
 
 
+# PLAN B
+#testet:
 
+# AF WE
+
+# original kode
+sorted = sort(fst_af_we_100$sliding_window, index.return = T, decreasing = T)
+sortedn = tibble(x = sorted$x, ix = sorted$ix + 49, pos = fst_af_we_100$pos[sorted$ix + 49]) # adjust for window size
+# nu kan det her indsættet i overlap med et range fra 1:n som giver et ønsket antal gener.
+# Det kan godt være at koden kan skrives mere effektivt med argumentet partial (?sort), jeg gider bare ikke teste det.
+
+old_af_we_all_peak_candidates = tibble(start = sortedn$pos,
+                                   end = sortedn$pos,
+                                   fst_rolwin_hecto = sortedn$x)
+
+# parametriseret udgave
+sort_fst = function(fst_region_100) {
+    # fst_region_100 needs to have a column called sliding_window with fst values offset 49 rows, and a pos column with the respective genome positions for fst values
+    sorted = sort(fst_region_100$sliding_window,
+                  index.return = T,
+                  decreasing = T)
+    sortedn = tibble(x = sorted$x,
+                     ix = sorted$ix + 49,
+                     pos = fst_region_100$pos[sorted$ix + 49]) # adjust for window size
+    # nu kan det her indsættet i overlap med et range fra 1:n som giver et ønsket antal gener.
+    # Det kan godt være at koden kan skrives mere effektivt med argumentet partial (?sort), jeg gider bare ikke teste det.
+    
+    return_variable = tibble(start = sortedn$pos,
+                          end = sortedn$pos,
+                          fst_rolwin_hecto = sortedn$x)
+    return(return_variable)
+}
+af_we_all_peak_candidates = sort_fst(fst_af_we_100)
+we_ea_all_peak_candidates = sort_fst(fst_we_ea_100)
+ea_af_all_peak_candidates = sort_fst(fst_ea_af_100)
 
 
 #   III:
@@ -172,17 +190,40 @@ gtf <- as.data.table(rtracklayer::import("data/gencode.v17.annotation.gtf"))
 gtf <- gtf[gtf$seqnames == 'chrX']   # to select only X chromosome
 
 
-
-
-
+# PLAN A
 # gammelt overlap fra manuelle fundne peaks.
-ovl_af_we = fst_overlap(as.data.table(af_we_peaks), gtf)
-unique(ovl_af_we$gene_name)
-View(ovl_af_we)
+overlap_af_we = fst_overlap(as.data.table(af_we_peaks), gtf)
+overlap_af_we
+View(overlap_af_we)
+
+
+# PLAN B
+
+# AF WE
+lim = 12000
+new_overlap_af_we = fst_overlap(as.data.table(af_we_all_peak_candidates[1:lim,]), gtf[1:lim,])
+simple = new_overlap_af_we[,c(1, 2, 3, 15, 17, 18, 23)]
+unique(new_overlap_af_we$gene_name)
+
+
+# WE EA
+lim = 12000
+new_overlap_we_ea = fst_overlap(as.data.table(we_ea_all_peak_candidates[1:lim,]), gtf[1:lim,])
+simple = new_overlap_we_ea[,c(1, 2, 3, 15, 17, 18, 23)]
+unique(new_overlap_we_ea$gene_name)
+
+
+# EA AF
+lim = 12000
+new_overlap_ea_af = fst_overlap(as.data.table(ea_af_all_peak_candidates[1:lim,]), gtf[1:lim,])
+simple = new_overlap_ea_af[,c(1, 2, 3, 15, 17, 18, 23)]
+unique(new_overlap_ea_af$gene_name)
+
+
 
 
 #   IV:
-# -----
+# ------
 # Discuss potential adaptive explanations.
 
 
