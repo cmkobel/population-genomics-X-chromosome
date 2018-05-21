1e6416c6d1e31e36e629aee568a2ecbf29d1035f
Carl M. Kobel <kobel@pm.me>
2018-05-21 15:24:05 +0200

    quantile works

--- projX a.r
+++ projX a.r
@@ -47,24 +47,25 @@ fst_rolling_window = function(pos, freq_A_1, freq_A_2, size, mfunction) {
 }
 
 # FST: Jeg er ikke sikker på at dette er den rigtige måde at gøre det på. Da jeg ikke skalerer med antallet af individer
-# window 1000
+# window 1000, only needed for plotting
 fst_af_we = fst_rolling_window(res_scan_AF$POSITION, res_scan_AF$freq_A, res_scan_WE$freq_A, 1000, cmean)
 fst_we_ea = fst_rolling_window(res_scan_AF$POSITION, res_scan_WE$freq_A, res_scan_EA$freq_A, 1000, cmean)
 fst_ea_af = fst_rolling_window(res_scan_AF$POSITION, res_scan_EA$freq_A, res_scan_AF$freq_A, 1000, cmean)
 
 
-# window 100
+# window 100, for statistics
 fst_af_we_100 = fst_rolling_window(res_scan_AF$POSITION, res_scan_AF$freq_A, res_scan_WE$freq_A, 100, cmean)
 fst_we_ea_100 = fst_rolling_window(res_scan_AF$POSITION, res_scan_WE$freq_A, res_scan_EA$freq_A, 100, cmean)
 fst_ea_af_100 = fst_rolling_window(res_scan_AF$POSITION, res_scan_EA$freq_A, res_scan_AF$freq_A, 100, cmean)
 
 
-# leg med densitet og percentiler
-quantile(fst_af_we_100$sliding_window, 0.95, na.rm = T)
-
-plot(density(na.omit(fst_af_we_100$sliding_window)))
-abline(v=0.1607412)
-abline(v=0.1172794)
+# # leg med densitet og percentiler
+# quantile(fst_af_we_100$sliding_window, 0.95, na.rm = T)
+# 
+# # Ville det ikke være smart at smide densitets plot ind, måske nede i plan B2?
+# plot(density(na.omit(fst_af_we_100$sliding_window)))
+# abline(v=0.1607412)
+# abline(v=0.1172794)
 
 
 
@@ -116,45 +117,45 @@ dev.off()
 
 
 
-# PLAN A
-# Denne funktion bruges ikke hvis sorteringsmeoden er frugtbar
-cut_fst = function(df_fst, n, return = FALSE) {
-    sorted = sort(df_fst$sliding_window, index.return = T, decreasing = T)
-    # AF_WE, 7000 seems nice
-    result = cbind(c(365712, df_fst$pos[sorted$ix[1:n]], 155110877), c(0.0, sorted$x[1:n], 0.0))
-    if (return) return(result)
-    else print(plot(result, cex = 0.3))
-}
+# # PLAN A
+# # Denne funktion bruges ikke hvis sorteringsmeoden er frugtbar
+# cut_fst = function(df_fst, n, return = FALSE) {
+#     sorted = sort(df_fst$sliding_window, index.return = T, decreasing = T)
+#     # AF_WE, 7000 seems nice
+#     result = cbind(c(365712, df_fst$pos[sorted$ix[1:n]], 155110877), c(0.0, sorted$x[1:n], 0.0))
+#     if (return) return(result)
+#     else print(plot(result, cex = 0.3))
+# }
 
-
-
-# AF WE
-#cut_fst(fst_af_we, 5000)
-# Comment on window size: Because we want to finde parts of genes having high Fst, we need to look in windows 
-af_we_thresh = 310; cut_fst(fst_af_we_100, af_we_thresh)
-#write.table(cut_fst(fst_af_we_100, af_we_thresh, T), file = "10_peaks_fst_af_we.tsv")
-af_we_peaks = tibble(start = c(19182660, 37878268, 46095748, 55959471, 66270950, 92414472, 104551933, 140734729, 141641747, 145276227),
-                     end = start,
-                     fst = c(0.2502342793, 0.2194004233, 0.2158843816, 0.2101256287, 0.2144259546, 0.2463066009, 0.2330120671, 0.2172732175, 0.2584422211, 0.214525942))
-plot(af_we_peaks)
-
-
-# WE EA <
-we_ea_thresh = 700; cut_fst(fst_we_ea_100, we_ea_thresh) # plot
-#write.table(cut_fst(fst_we_ea_100, we_ea_thresh, T), file = "10_peaks_fst_we_ea.tsv")
-we_ea_peaks = tibble(start = c(25671846, 42579633, 71373407, 73295813, 74119733, 87742329, 108285879, 109355662, 116617318, 128684757),
-                     end = start,
-                      fst = c(0.2626619986, 0.2428685249, 0.2935684424, 0.3036629517, 0.2230540469, 0.2677614183, 0.2119459508, 0.2461968528, 0.2129359005, 0.2161413781))
-plot(we_ea_peaks)
-
-
-# EA AF
-ea_af_thresh = 870; cut_fst(fst_ea_af_100, ea_af_thresh)
-#write.table(cut_fst(fst_ea_af_100, ea_af_thresh, T), file = "10_peaks_fst_ea_af.tsv")
-ea_af_peaks = tibble(start = c(19183926, 36636363, 55960598, 63969563, 66270950, 90576998, 112894056, 121069027, 124687575, 126784188),
-                     end = start,
-                     fst = c(0.2697129718, 0.3670665736, 0.2939780025, 0.2757454847, 0.390637325, 0.3747966217, 0.2606229121, 0.2809754149, 0.2643502852, 0.3067912248))
-plot(ea_af_peaks[2:3])
+# 
+# 
+# # AF WE
+# #cut_fst(fst_af_we, 5000)
+# # Comment on window size: Because we want to finde parts of genes having high Fst, we need to look in windows 
+# af_we_thresh = 310; cut_fst(fst_af_we_100, af_we_thresh)
+# #write.table(cut_fst(fst_af_we_100, af_we_thresh, T), file = "10_peaks_fst_af_we.tsv")
+# af_we_peaks = tibble(start = c(19182660, 37878268, 46095748, 55959471, 66270950, 92414472, 104551933, 140734729, 141641747, 145276227),
+#                      end = start,
+#                      fst = c(0.2502342793, 0.2194004233, 0.2158843816, 0.2101256287, 0.2144259546, 0.2463066009, 0.2330120671, 0.2172732175, 0.2584422211, 0.214525942))
+# plot(af_we_peaks)
+# 
+# 
+# # WE EA <
+# we_ea_thresh = 700; cut_fst(fst_we_ea_100, we_ea_thresh) # plot
+# #write.table(cut_fst(fst_we_ea_100, we_ea_thresh, T), file = "10_peaks_fst_we_ea.tsv")
+# we_ea_peaks = tibble(start = c(25671846, 42579633, 71373407, 73295813, 74119733, 87742329, 108285879, 109355662, 116617318, 128684757),
+#                      end = start,
+#                       fst = c(0.2626619986, 0.2428685249, 0.2935684424, 0.3036629517, 0.2230540469, 0.2677614183, 0.2119459508, 0.2461968528, 0.2129359005, 0.2161413781))
+# plot(we_ea_peaks)
+# 
+# 
+# # EA AF
+# ea_af_thresh = 870; cut_fst(fst_ea_af_100, ea_af_thresh)
+# #write.table(cut_fst(fst_ea_af_100, ea_af_thresh, T), file = "10_peaks_fst_ea_af.tsv")
+# ea_af_peaks = tibble(start = c(19183926, 36636363, 55960598, 63969563, 66270950, 90576998, 112894056, 121069027, 124687575, 126784188),
+#                      end = start,
+#                      fst = c(0.2697129718, 0.3670665736, 0.2939780025, 0.2757454847, 0.390637325, 0.3747966217, 0.2606229121, 0.2809754149, 0.2643502852, 0.3067912248))
+# plot(ea_af_peaks[2:3])
 
 
 
@@ -163,36 +164,51 @@ plot(ea_af_peaks[2:3])
 
 # AF WE
 
-# original kode
-sorted = sort(fst_af_we_100$sliding_window, index.return = T, decreasing = T)
-sortedn = tibble(x = sorted$x, ix = sorted$ix + 49, pos = fst_af_we_100$pos[sorted$ix + 49]) # adjust for window size
-# nu kan det her indsættet i overlap med et range fra 1:n som giver et ønsket antal gener.
-# Det kan godt være at koden kan skrives mere effektivt med argumentet partial (?sort), jeg gider bare ikke teste det.
-
-old_af_we_all_peak_candidates = tibble(start = sortedn$pos,
-                                   end = sortedn$pos,
-                                   fst_rolwin_hecto = sortedn$x)
+# # original kode
+# sorted = sort(fst_af_we_100$sliding_window, index.return = T, decreasing = T)
+# sortedn = tibble(x = sorted$x, ix = sorted$ix + 49, pos = fst_af_we_100$pos[sorted$ix + 49]) # adjust for window size
+# # nu kan det her indsættet i overlap med et range fra 1:n som giver et ønsket antal gener.
+# # Det kan godt være at koden kan skrives mere effektivt med argumentet partial (?sort), jeg gider bare ikke teste det.
+# 
+# old_af_we_all_peak_candidates = tibble(start = sortedn$pos,
+#                                    end = sortedn$pos,
+#                                    fst_rolwin_hecto = sortedn$x)
 
 # parametriseret udgave
-sort_fst = function(fst_region_100) {
-    # fst_region_100 needs to have a column called sliding_window with fst values offset 49 rows, and a pos column with the respective genome positions for fst values
-    sorted = sort(fst_region_100$sliding_window,
-                  index.return = T,
-                  decreasing = T)
-    sortedn = tibble(x = sorted$x,
-                     ix = sorted$ix + 49,
-                     pos = fst_region_100$pos[sorted$ix + 49]) # adjust for window size
-    # nu kan det her indsættet i overlap med et range fra 1:n som giver et ønsket antal gener.
-    # Det kan godt være at koden kan skrives mere effektivt med argumentet partial (?sort), jeg gider bare ikke teste det.
-    
-    return_variable = tibble(start = sortedn$pos,
-                          end = sortedn$pos,
-                          fst_rolwin_hecto = sortedn$x)
-    return(return_variable)
+# sort_fst = function(fst_region_100) {
+#     # fst_region_100 needs to have a column called sliding_window with fst values offset 49 rows, and a pos column with the respective genome positions for fst values
+#     sorted = sort(fst_region_100$sliding_window,
+#                   index.return = T,
+#                   decreasing = T)
+#     sortedn = tibble(x = sorted$x,
+#                      ix = sorted$ix + 49,
+#                      pos = fst_region_100$pos[sorted$ix + 49]) # adjust for window size
+#     # nu kan det her indsættet i overlap med et range fra 1:n som giver et ønsket antal gener.
+#     # Det kan godt være at koden kan skrives mere effektivt med argumentet partial (?sort), jeg gider bare ikke teste det.
+#     
+#     return_variable = tibble(start = sortedn$pos,
+#                           end = sortedn$pos,
+#                           fst_rolwin_hecto = sortedn$x)
+#     return(return_variable)
+# }
+# af_we_all_peak_candidates = sort_fst(fst_af_we_100)
+# we_ea_all_peak_candidates = sort_fst(fst_we_ea_100)
+# ea_af_all_peak_candidates = sort_fst(fst_ea_af_100)
+
+# sort_fst behøver ikke at sortere, men det er meget rart at have sliding window og positioner (star = end) sammen
+not_sort_fst = function(fst_region_100) {
+    return(
+        na.omit(
+            tibble(start = fst_region_100$pos,
+                   end = start,
+                   fst_rolwin_hecto = fst_region_100$sliding_window)
+            )
+        )
 }
-af_we_all_peak_candidates = sort_fst(fst_af_we_100)
-we_ea_all_peak_candidates = sort_fst(fst_we_ea_100)
-ea_af_all_peak_candidates = sort_fst(fst_ea_af_100)
+
+af_we_all_peak_candidates = not_sort_fst(fst_af_we_100)
+we_ea_all_peak_candidates = not_sort_fst(fst_we_ea_100)
+ea_af_all_peak_candidates = not_sort_fst(fst_ea_af_100)
 
 
 #   III:
@@ -217,38 +233,38 @@ gtf <- gtf[gtf$seqnames == 'chrX']   # to select only X chromosome
 
 
 # PLAN A
-# gammelt overlap fra manuelle fundne peaks.
-overlap_af_we = fst_overlap(as.data.table(af_we_peaks), gtf)
-overlap_af_we
-View(overlap_af_we)
+# # gammelt overlap fra manuelle fundne peaks.
+# overlap_af_we = fst_overlap(as.data.table(af_we_peaks), gtf) # det er vel ikke nødvendigt at caste til data.table hvis det allerede er gjort 5 linjer tilbage??
+# overlap_af_we
+# View(overlap_af_we)
 
 
 # PLAN B2 (percentiler), B1 findes som et historisk commit på github or below
 
 # AF WE
+# percentile = 0.998
+# threshold = quantile(na.omit(fst_af_we_100$sliding_window), percentile) 
+# new_overlap_af_we = fst_overlap(as.data.table(af_we_all_peak_candidates[af_we_all_peak_candidates$fst_rolwin_hecto >= threshold,]), gtf) # fst_af_we_100 burde kunne bruges lige så vel som reg_all_peak_cand.
+# #View(new_overlap_af_we)
+# unique(cbind(new_overlap_af_we$gene_name, new_overlap_af_we$start))
+# 
+# peak_result = new_overlap_af_we %>%
+#     group_by(gene_name) %>% 
+#     summarise(position = start[which.max(fst_rolwin_hecto)],
+#               fst_peak = max(fst_rolwin_hecto),
+#               transcript_type = transcript_type[which.max(fst_rolwin_hecto)]) # inserted in text
+
+# AF WE uden sort (i stedet med percentil)
 percentile = 0.998
-threshold = quantile(na.omit(fst_af_we_100$sliding_window), percentile) 
+threshold = quantile(af_we_all_peak_candidates$fst_rolwin_hecto, percentile)
 new_overlap_af_we = fst_overlap(as.data.table(af_we_all_peak_candidates[af_we_all_peak_candidates$fst_rolwin_hecto >= threshold,]), gtf) # fst_af_we_100 burde kunne bruges lige så vel som reg_all_peak_cand.
 #View(new_overlap_af_we)
-unique(cbind(new_overlap_af_we$gene_name, new_overlap_af_we$start))
+unique(cbind(new_overlap_af_we$gene_name))
 
 peak_result = new_overlap_af_we %>%
-    group_by(gene_name) %>% 
+    group_by(gene_name) %>%
     summarise(position = start[which.max(fst_rolwin_hecto)],
               fst_peak = max(fst_rolwin_hecto),
-              transcript_type = transcript_type[which.max(fst_rolwin_hecto)]) # inserted in text
-
-# AF WE uden sort
-percentile = 0.998
-threshold = quantile(na.omit(fst_af_we_100$sliding_window), percentile) 
-new_overlap_af_we = fst_overlap(as.data.table(fst_af_we_100[fst_af_we_100$fst_rolwin_hecto >= threshold,]), gtf) # fst_af_we_100 burde kunne bruges lige så vel som reg_all_peak_cand.
-#View(new_overlap_af_we)
-unique(cbind(new_overlap_af_we$gene_name, new_overlap_af_we$start))
-
-peak_result = new_overlap_af_we %>%
-    group_by(gene_name) %>% 
-    summarise(position = start[which.max(fst_rolwin_hecto)],
-              new_fst_peak = max(fst_rolwin_hecto),
               transcript_type = transcript_type[which.max(fst_rolwin_hecto)]) # inserted in text
 
 
